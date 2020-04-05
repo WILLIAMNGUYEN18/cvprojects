2551d53be0c745ad0d811f491378ded65da4308e
William Nguyen <nguyew2@uw.edu>
2020-04-04 20:29:13 -0700

    Baseline

---
+++ valorantcv/valorantcv/ParseMP4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace valorantcv
+{
+    class ParseMP4
+    {
+
+        public static void FFMPEGProcessFrames()
+        {
+            //Directory.CreateDirectory();
+        }
+
+
+        //parse frames
+        private static string parseFrames(double interval)
+        {
+
+            return "";
+        }
+        
+    }
+}
