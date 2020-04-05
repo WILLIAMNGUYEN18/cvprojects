b6f39f4b46f8babceab78597616e50ff908196d2
William Nguyen <nguyew2@uw.edu>
2020-04-05 03:05:17 -0700

    was able to do an example run of ffmpeg to parse 1 frame per second using absolute paths. Frames parsed for G2 video.

--- valorantcv/valorantcv/ParseMP4.cs
+++ valorantcv/valorantcv/ParseMP4.cs
@@ -14,9 +14,11 @@ namespace valorantcv
         }
 
 
-        //parse frames
+        //parse frames based on a given interval
         private static string parseFrames(double interval)
         {
+
+            //ffmpeg -i "C:\valorantcv\rawfootage\g2valorant.mp4" -vf "fps=1/1,showinfo" "C:\valorantcv\outputFrames\frame-%05d.png"
 
             return "";
         }
