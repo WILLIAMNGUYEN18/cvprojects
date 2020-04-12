427aa2ac7e065f84ba47e1503133586ba18d9403
WILLIAMNGUYEN18 <nguyew2@uw.edu>
2020-04-11 21:52:40 -0700

    Started creating test cases. Some spike images are well below the threshold. (even at 0). Need to figure out why

--- valorantcv/valorantcv/Program.cs
+++ valorantcv/valorantcv/Program.cs
@@ -18,7 +18,8 @@ namespace valorantcv
 
             Console.WriteLine("Canny Match Frame Test Starting...");
             //prep
-            string spikeOriginalFrame = "C:\\valorantcv\\outputFrames\\frame-00121.png";
+            string spikeTestDefault = "C:\\valorantcv\\testBaseCanny\\spikeTest";
+
             string spikeTemplate = "C:\\valorantcv\\templates\\spikeactive.png";
             Rectangle spikeRect = new Rectangle(
                 (900),
@@ -27,15 +28,37 @@ namespace valorantcv
                 (100)
              );
 
-            string spikeOriginalOutput = "C:\\valorantcv\\testBaseCanny\\spikeTest.png";
+            //output
+            string spikeOriginalFrame = "C:\\valorantcv\\outputFrames\\frame-00121.png";
+            string spikeOriginalOutput = spikeTestDefault + parseFrameNumber(spikeOriginalFrame) + ".png";
+            ParseMP4.CannyMatchFrame(spikeOriginalFrame, spikeTemplate,spikeRect, 50, spikeOriginalOutput);
 
-            //Output
-            ParseMP4.CannyMatchFrame(spikeOriginalFrame, spikeTemplate,spikeRect, 50, spikeOriginalOutput, true);
+            string spikeRedCountdownFrame = "C:\\valorantcv\\outputFrames\\frame-00052.png";
+            string spikeRedOutput = spikeTestDefault + parseFrameNumber(spikeRedCountdownFrame) + ".png";
+            ParseMP4.CannyMatchFrame(spikeRedCountdownFrame,spikeTemplate, spikeRect, 50, spikeRedOutput);
 
+            string spikeDarkFrame = "C:\\valorantcv\\outputFrames\\frame-00124.png";
+            string spikeDarkOutput = spikeTestDefault + parseFrameNumber(spikeDarkFrame) + ".png";
+            ParseMP4.CannyMatchFrame(spikeDarkFrame, spikeTemplate, spikeRect, 50, spikeDarkOutput);
 
-            string spikeDifferentFrame ="";
+            string spikeMixedFrame = "C:\\valorantcv\\outputFrames\\frame-00127.png";
+            string spikeMixedOutput = spikeTestDefault + parseFrameNumber(spikeMixedFrame) + ".png";
+            ParseMP4.CannyMatchFrame(spikeMixedFrame, spikeTemplate, spikeRect, 50, spikeMixedOutput);
+
 
             Console.WriteLine("Canny Match Frame Test Complete!");
+            //Console.WriteLine(parseFrameNumber(spikeOriginalFrame));
+            //Console.WriteLine(parseFrameNumber(spikeRedCountdownFrame));
+        }
+
+        static string parseFrameNumber(string framePath) 
+        {
+            int parsingFrame = framePath.LastIndexOf('-');
+            string frame = framePath.Substring(parsingFrame + 1);
+            int trimPNG = frame.LastIndexOf('.');
+            frame = frame.Remove(trimPNG);
+
+            return frame;
         }
     }
 }
