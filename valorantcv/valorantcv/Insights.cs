53a12ace0b17efb1220a3f1d0839bfee4fafb5bf
William Nguyen <nguyew2@uw.edu>
2020-04-30 21:31:56 -0700

    Making basic timeline. Indexing seems a bit off. May have it become a uniform (1 hour timeline image?). Image is slightly larger than actualy timeline outline. Improves the larger it is (less variance from instanceSize decimals).

--- valorantcv/valorantcv/Insights.cs
+++ valorantcv/valorantcv/Insights.cs
@@ -24,8 +24,7 @@ namespace valorantcv
             //parse the file path
             List<TimelineInstance> Timeline = JsonConvert.DeserializeObject<List<TimelineInstance>>(File.ReadAllText(filePath));
 
-            //scale image by list length;
-            int scale = Timeline.Count;
+
 
 
             //Could use System.Drawing, will try out emgu drawings first
@@ -33,11 +32,58 @@ namespace valorantcv
             //since we have frames (and eventually time) tied to each spike event
             //Create an image of set size
             //potentially refactor into its own draw method
-            Image<Bgr, byte> Slate = new Image<Bgr, byte>(500, 200, new Bgr(255,255,255));
-            
-            
-            
-            
+            Image<Bgr, byte> Slate = new Image<Bgr, byte>(1920, 200, new Bgr(255,255,255));
+
+
+            Rectangle outline = new Rectangle
+                (
+                50,
+                75,
+                1820,
+                50
+                );
+            Slate.Draw(outline, new Bgr(0,0,0),2);
+
+            //scale image by list length;
+            int scale = Timeline.Count;
+
+            //need increment
+            int count = 0;
+
+            foreach (TimelineInstance time in Timeline)
+            {
+                Bgr timelineColor;
+                //handle color of (red for spike, grey for normal)
+                if (time.spike)
+                {
+                    timelineColor = new Bgr(Color.Red);
+                }
+                else
+                {
+                    timelineColor = new Bgr(Color.LightGray);
+                }
+
+                double instanceSize = (outline.Width / scale);
+                //int currX = count * (int)Math.Ceiling(instanceSize) + count;
+                int currX = count * (int)instanceSize + count;
+
+                Console.WriteLine(currX);
+                Console.WriteLine(instanceSize);
+                Console.WriteLine(currX + instanceSize);
+                Console.WriteLine();
+
+                // starting position is 50
+                // increment is count * imgSize/scale
+                // If thickness is less than 1, the rectangle is filled up
+                Rectangle currentRec = new Rectangle(51 + currX, 77, (int)instanceSize, 46); 
+                Slate.Draw(currentRec,timelineColor,-1);
+                count++;
+
+            }
+
+            Slate.Save("C:\\valorantcv\\testBaseCanny\\testViz.png");
+
+            //for each index, in the timeline, draw a box
             //create an event list that we care about
             //loop across all the frames
 
